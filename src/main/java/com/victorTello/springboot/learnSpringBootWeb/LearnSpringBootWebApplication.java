556eb826817d97cd006f37979415d2c27259cda2
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-05-25 15:36:34 -0600

    first commit

---
+++ src/main/java/com/victorTello/springboot/learnSpringBootWeb/LearnSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.victorTello.springboot.learnSpringBootWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorTello/springboot/learnSpringBootWeb/LearnSpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorTello.springboot.learnSpringBootWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
